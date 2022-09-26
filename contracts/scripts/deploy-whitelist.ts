6c7c20eb45dbcadd706f85e2c1a4a1171d6436b9
Azamat Zulpykhar <augustintherome@gmail.com>
2022-09-26 15:50:58 +0600

    feat: deployed contract

---
+++ contracts/scripts/deploy-whitelist.ts
@@ -0,0 +1,17 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const Whitelist = await ethers.getContractFactory("Whitelist");
+  const whitelist = await Whitelist.deploy(10);
+
+  await whitelist.deployed();
+
+  console.log(`Whitelist deployed to ${whitelist.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
