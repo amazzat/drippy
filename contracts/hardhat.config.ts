dfd3622ac2a74f518bb4b5178d8a72fea7595f59
Azamat Zulpykhar <augustintherome@gmail.com>
2022-10-01 03:11:55 +0600

    feat: done

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -8,10 +8,10 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY!;
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
-    goerli: {
-      url: QUICKNODE_HTTP_URL,
-      accounts: [PRIVATE_KEY],
-    },
+    // goerli: {
+    //   url: QUICKNODE_HTTP_URL,
+    //   accounts: [PRIVATE_KEY],
+    // },
   },
 };
 
--- web/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Routes } from "@blitzjs/next";
-import Link from "next/link";
-
-export default function Index() {
-  return <Link href={Routes.Index()}>Index</Link>;
-}
